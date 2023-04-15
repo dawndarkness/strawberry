5ddbc38f5300b248f2c95efbf87223a7e400e2c1
Julie <julie@bitcoin.com>
2023-04-15 11:30:01 +0900

    add a green palette

--- data/palettes.js
+++ data/palettes.js
@@ -1,5 +1,6 @@
 export const colorPalettes = {
     palette1: ["#845EC2", "#4B4453", "#B0A8B9", "#C34A36", "#FF8066"],
     palette2: ["#699674", "#3B4A3F", "#9EAFA2", "#4E94B3", "#03617E"],
-    palette3: ["#27B6D8", "#334A52", "#96AFB8", "#C498D4", "#8E649D"]
+    palette3: ["#27B6D8", "#334A52", "#96AFB8", "#C498D4", "#8E649D"],
+    palette4: ["#5AA55B", "#24956D", "#008377", "#006F77", "#1C5B6C"]
 }
