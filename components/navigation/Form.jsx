12f1ce1f87b37881279c31babb0a6225f6a7a32c
Bolun <53603127+zblenlighten@users.noreply.github.com>
2023-04-15 21:39:06 +0900

    remove usd value (#16)

--- components/navigation/Form.jsx
+++ components/navigation/Form.jsx
@@ -4,14 +4,14 @@ import { ethers } from 'ethers';
 import StrawberryCharity from '../../strawberryContract/contracts/artifacts/StrawberryCharity.json';
 
 const Form = () => {
-    const [ donationAmount, setDonationAmount] = useState('');
+    const [donationAmount, setDonationAmount] = useState('');
 
     // get contract address
     const CONTRACT_ADDRESS = "0x102482DD4e494a8c1341725cc29ecC557D705E1b";
 
     const CONTRACT_ABI = StrawberryCharity.abi;
 
-     const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         // e.preventDefault();
         console.log(e)
     }
@@ -20,22 +20,21 @@ const Form = () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
-    
+
         const donationAmountInWei = ethers.utils.parseEther(donationAmount);
         const transaction = await contract.donate({ value: donationAmountInWei, gasLimit: 50000 });
-    
+
         await transaction.wait();
-    
+
         setDonationAmount(0);
-      };
+    };
 
     return (
         <div className={styles.form_container}>
             <div className={styles.form_container}>
-                <input placeholder="Enter amount in ETH" type="number" name="amount" value={donationAmount} onChange={e => setDonationAmount(e.target.value)} className={styles.input}/>
+                <input placeholder="Enter amount in ETH" type="number" name="amount" value={donationAmount} onChange={e => setDonationAmount(e.target.value)} className={styles.input} />
                 <button onClick={handleDonate} className={styles.donate_button}>Donate</button>
             </div>
-            {`$${donationAmount * 2000}`}
         </div>
     )
 }
