966edd0b9eb17b40695d1835c4bfa9efdceb64c7
Julie <julie@bitcoin.com>
2023-04-15 16:02:13 +0900

    change typo

--- data/npo.js
+++ data/npo.js
@@ -14,7 +14,7 @@ export const NPO_LIST = [
     {
         id: "0x69EBe456568948713CB6761D220a5c58E1315F39",
         name: "Shrey's Shield",
-        description: "Shrey's shield has been providing meals to children in low poverty areas for over 10 years. A donation of just $10 can feed one child for a week.",
+        description: "Shrey's shield has been providing meals to children in low income areas for over 10 years. A donation of just $10 can feed one child for a week.",
         image: "npo/shrey_shield.png"
     },
 ]
